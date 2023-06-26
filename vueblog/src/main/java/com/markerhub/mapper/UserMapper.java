0cc6916ab0e78a37dcceacf8dc69a3f0e87a027c
Paul-hao <94223306+Pual-hao@users.noreply.github.com>
2023-06-26 22:50:11 +0800

    Add files via upload

---
+++ vueblog/src/main/java/com/markerhub/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package com.markerhub.mapper;
+
+import com.markerhub.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author anonymous
+ * @since 2023-06-18
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
